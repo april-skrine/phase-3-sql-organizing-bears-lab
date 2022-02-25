fd645e8e3d9ee62765d325546df6b98bda41f482
April Skrine <april.skrine@gmail.com>
2022-02-25 10:01:35 -0600

    commit tree ac92ea0e78f51952c56b6034a4a6436dfa886a59

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 17, 'M', 'Brown', 'Pissy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 20, 'M', 'Black', 'Scared', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'White', 'Tired', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'Gray', 'Grumpy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 13, 'F', 'Gray', 'Grumpy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'Gray', 'Grumpy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'Brown', 'Pissy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 17, 'M', 'Brown', 'Pissy', 1);
